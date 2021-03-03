ff5ddaa88da0f8df9663460ff4d9893e5eb1e9a3
Cvqtko <cvqtkokirilov@gmail.com>
2021-03-03 16:44:19 +0200

    Exercises

--- 02_combinatorial_problems/src/Exercise_04.java
+++ 02_combinatorial_problems/src/Exercise_04.java
@@ -2,8 +2,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise_04 {
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		char[] elements = scanner.nextLine().replaceAll("\\s+", "").toCharArray();
+		int k = Integer.parseInt(scanner.nextLine());
+		char[] variations = new char[k];
+
+		variations(0, elements, variations);
+	}
+
+	public static void variations(int index, char[] elements, char[] variations) {
+		if (index == variations.length) {
+			print(variations);
+			return;
+		} else {
+			for (int i = 0; i < elements.length; i++) {
+
+				variations[index] = elements[i];
+				variations(index + 1, elements, variations);
+
+			}
+		}
+	}
+
+	private static void print(char[] perm) {
+		System.out.println(new String(perm));
 
 	}
+
 }
