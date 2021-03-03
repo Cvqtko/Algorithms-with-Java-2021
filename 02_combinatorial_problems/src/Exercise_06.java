78ad3891af95b4f3d18afa4d140b0cab23a6e0d1
Cvqtko <cvqtkokirilov@gmail.com>
2021-03-03 17:18:02 +0200

    Exercises

--- 02_combinatorial_problems/src/Exercise_06.java
+++ 02_combinatorial_problems/src/Exercise_06.java
@@ -2,8 +2,31 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise_06 {
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		char[] elements = scanner.nextLine().replaceAll("\\s+", "").toCharArray();
+		int k = Integer.parseInt(scanner.nextLine());
+		char[] combinations = new char[k];
+
+		combinations(0, 0, elements,combinations);
+	}
+
+	public static void combinations(int index,int start, char[] elements, char[] combinations) {
+		if (index == combinations.length) {
+			print(combinations);
+		} else {
+			for (int i = start; i < elements.length; i++) {
+				combinations[index] = elements[i];
+				combinations(index + 1,i, elements, combinations);
+
+			}
+		}
+	}
+
+	private static void print(char[] perm) {
+		System.out.println(new String(perm));
 
 	}
+
 }
