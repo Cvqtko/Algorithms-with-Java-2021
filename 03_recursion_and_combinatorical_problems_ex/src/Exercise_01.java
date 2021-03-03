70e23ce17a84713f61f440aed031bd099db7a0d0
Cvqtko <cvqtkokirilov@gmail.com>
2021-03-03 18:09:26 +0200

    Exercises

--- 03_recursion_and_combinatorical_problems_ex/src/Exercise_01.java
+++ 03_recursion_and_combinatorical_problems_ex/src/Exercise_01.java
@@ -1,9 +1,22 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Exercise_01 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] elements = reader.readLine().split("\\s+");
+
+		printReversed(elements, elements.length - 1);
+	}
+
+	private static void printReversed(String[] elements, int index) {
+		if (index < 0) {
+			return;
+		}
+		System.out.print(elements[index] + " ");
+		printReversed(elements, index - 1);
 
 	}
 }
