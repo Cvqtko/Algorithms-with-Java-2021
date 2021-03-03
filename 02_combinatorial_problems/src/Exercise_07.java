619c50a43e720f1e263edcd35e45ed489b03860e
Cvqtko <cvqtkokirilov@gmail.com>
2021-03-03 17:29:00 +0200

    Exercises

--- 02_combinatorial_problems/src/Exercise_07.java
+++ 02_combinatorial_problems/src/Exercise_07.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 public class Exercise_07 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		int n = Integer.parseInt(scanner.nextLine());
+		int k = Integer.parseInt(scanner.nextLine());
 
+		System.out.println(binom(n, k));
 	}
+
+	public static int binom(int n, int k) {
+		if (k > n)
+			return 0;
+		if (k == 0 || k == n)
+			return 1;
+		return binom(n - 1, k - 1) + binom(n - 1, k);
+	}
+
 }
