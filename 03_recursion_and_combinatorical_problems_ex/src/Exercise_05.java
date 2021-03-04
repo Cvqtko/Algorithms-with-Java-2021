4ac613b684417f55efc34c648cd0c4e426f367df
Cvqtko <cvqtkokirilov@gmail.com>
2021-03-04 14:31:25 +0200

    Exercises

--- 03_recursion_and_combinatorical_problems_ex/src/Exercise_05.java
+++ 03_recursion_and_combinatorical_problems_ex/src/Exercise_05.java
@@ -1,9 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise_05 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 
+	public static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(reader.readLine());
+		int k = Integer.parseInt(reader.readLine());
+		arr = new int[k];
+
+		combinations(n, 0, 1);
+	}
+
+	private static void combinations(int n, int index, int start) {
+		if (index == arr.length) {
+			print(arr);
+		} else {
+			for (int i = start; i <= n; i++) {
+				arr[index] = i;
+				combinations(n, index + 1, i + 1);
+			}
+		}
+
+	}
+
+	private static void print(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
 	}
 }
