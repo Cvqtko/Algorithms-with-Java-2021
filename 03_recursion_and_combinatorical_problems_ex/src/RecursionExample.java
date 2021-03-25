8277b54fdc229e928ad608a82e9161b6439209df
Cvqtko <cvqtkokirilov@gmail.com>
2021-03-25 12:42:00 +0200

    Exercises

---
+++ 03_recursion_and_combinatorical_problems_ex/src/RecursionExample.java
@@ -0,0 +1,18 @@
+
+public class RecursionExample {
+	static int count = 0;
+
+	static void p() {
+		count++;
+		if (count == 5) {
+			return;
+		} else {
+			System.out.println("hello " + count);
+			p();
+		}
+	}
+
+	public static void main(String[] args) {
+		p();
+	}
+}
