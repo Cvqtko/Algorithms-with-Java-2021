80f5e121ec5e2c31174780cb5849a4d3fd0171ac
Cvqtko <cvqtkokirilov@gmail.com>
2021-03-26 12:01:56 +0200

    Exercises

--- 03_recursion_and_combinatorical_problems_ex/src/Recursion.java
+++ 03_recursion_and_combinatorical_problems_ex/src/Recursion.java
@@ -2,17 +2,28 @@
 public class Recursion {
 	static int count = 0;
 
-	static void p() {
+	public static void main(String[] args) {
+		recursionOne();
+		recursionTwo(0);
+	}
+
+	static void recursionOne() {
 		count++;
 		if (count == 5) {
 			return;
 		} else {
 			System.out.println("hello " + count);
-			p();
+			recursionOne();
 		}
 	}
 
-	public static void main(String[] args) {
-		p();
+	static void recursionTwo(int count) {
+		if (count == 5) {
+			return;
+		} else {
+			System.out.println("hello " + count);
+			count++;
+			recursionTwo(count);
+		}
 	}
 }
