051adb4002d99abdfde71c0d7be2bb79a7ada66f
Cvqtko <cvqtkokirilov@gmail.com>
2021-03-27 21:55:58 +0200

    Exercises

--- 04_searching_sorting_greedy_algorithms/src/Exercise_04.java
+++ 04_searching_sorting_greedy_algorithms/src/Exercise_04.java
@@ -1,9 +1,48 @@
-import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Exercise_04 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		String[] elements = scanner.nextLine().substring(7).split(", ");
+		int[] coins = new int[elements.length];
 
+		for (int i = 0; i < elements.length; i++) {
+			coins[i] = Integer.parseInt(elements[i]);
+		}
+		int targetSum = Integer.parseInt(scanner.nextLine().substring(5));
+
+		Map<Integer, Integer> usedCoins;
+		try {
+			usedCoins = chooseCoins(coins, targetSum);
+			for (Entry<Integer, Integer> usedCoin : usedCoins.entrySet()) {
+				System.out.println(usedCoin.getKey() + "->" + usedCoin.getValue());
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+
+
+	}
+
+	private static Map<Integer, Integer> chooseCoins(int[] coins, int targetSum) throws Exception {
+		Map<Integer, Integer> usedCoins = new LinkedHashMap<Integer, Integer>();
+		int index = coins.length - 1;
+		while (targetSum != 0) {
+			if(index<0) {
+				throw new IllegalArgumentException("Error");
+			}
+			int coinsToTake = targetSum / coins[index];
+			if (coinsToTake != 0) {
+				usedCoins.put(coins[index], coinsToTake);
+			}
+			targetSum %= coins[index];
+			index--;
+			
+		}
+		return usedCoins;
 	}
 }
--- 04_searching_sorting_greedy_algorithms/src/Exercise_05.java
+++ 04_searching_sorting_greedy_algorithms/src/Exercise_05.java
@@ -1,9 +1,89 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Exercise_05 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		String[] universeInput = scanner.nextLine().substring(10).split(", ");
+		System.out.println();
+
+		List<Integer> universe = new ArrayList<Integer>();
+
+		for (String uni : universeInput) {
+			universe.add(Integer.parseInt(uni));
+		}
+
+		int numberOfSets = Integer.parseInt(scanner.nextLine().substring(16));
+		List<int[]> sets = new ArrayList<int[]>();
+		while (numberOfSets-- > 0) {
+			int[] currentSet = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+			sets.add(currentSet);
+		}
+		List<int[]> chosenSets = chooseSets(sets, universe);
+		System.out.printf("Sets to take (%d):\n",chosenSets.size());
+		for (int[] chosen : chosenSets) {
+			System.out.print("{ ");
+			for (int i = 0; i < chosen.length - 2; i++) {
+				System.out.print(chosen[i] + ", ");
+			}
+			System.out.println(chosen[chosen.length - 1] + " }");
+		}
+	}
+
+	public static List<int[]> chooseSets(List<int[]> sets, List<Integer> universe) {
+
+		List<int[]> selectedSets = new ArrayList<>();
+
+		Set<Integer> universeSet = new HashSet<>();
+
+		for (int element : universe) {
+			universeSet.add(element);
+		}
+
+		while (!universeSet.isEmpty()) {
+			int notChosenCount = 0;
+
+			int[] chosenSet = sets.get(0);
+
+			for (int[] set : sets) {
+
+				int count = 0;
+
+				for (int elem : set) {
+
+					if (universeSet.contains(elem)) {
+
+						count++;
+
+					}
+
+				}
+
+				if (notChosenCount < count) {
+
+					notChosenCount = count;
+
+					chosenSet = set;
+
+				}
+
+			}
+
+			selectedSets.add(chosenSet);
+
+			for (int elem : chosenSet) {
+
+				universeSet.remove(elem);
+
+			}
+
+		}
+
+		return selectedSets;
 
 	}
 }
