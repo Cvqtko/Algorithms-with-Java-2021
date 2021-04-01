f5d41dfc425d7eea210c052723f4cb2752fab508
Cvqtko <cvqtkokirilov@gmail.com>
2021-04-01 17:11:14 +0300

    Exercises

--- 07_dynamic_programming/src/Exercise_01.java
+++ 07_dynamic_programming/src/Exercise_01.java
@@ -2,8 +2,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise_01 {
+
+	public static long[] dp;
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		int n = scanner.nextInt();
+		dp = new long[n + 1];
+
+		long fib = calcFib(n);
+		System.out.println(fib);
+	}
+
+	private static long calcFib(int n) {
+		if (n <= 2) {
+			return 1;
+		}
+		
+		if(dp[n]!=0) {
+			return dp[n];
+		}
+		
+		return dp[n] = calcFib(n - 1) + calcFib(n - 2);
 	}
 }
