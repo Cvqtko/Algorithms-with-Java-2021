64af6b9c3e36f40d6bfedfd0dc1edf8582cf9d31
Cvqtko <cvqtkokirilov@gmail.com>
2021-04-05 12:38:24 +0300

    Exercises

--- 09_exam_preparation/src/Exercise_01.java
+++ 09_exam_preparation/src/Exercise_01.java
@@ -1,9 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise_01 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 
+	public static int n;
+	public static int[] expression;
+	public static int solutions;
+	public static StringBuilder builder = new StringBuilder();
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(reader.readLine());
+		expression = new int[n];
+		solutions = 0;
+		combinationsWithoutRep(0, 1);
+		builder.append("Total Solutions: " + solutions);
+		System.out.println(builder.toString());
+	}
+
+	private static void combinationsWithoutRep(int index, int start) {
+		if (index >= n) {
+			printSolution();
+		} else {
+
+			for (int i = start; i <= n; i++) {
+				expression[index] = i;
+				combinationsWithoutRep(index + 1, i + 1);
+				expression[index] = -i;
+				combinationsWithoutRep(index + 1, i + 1);
+			}
+		}
+
+	}
+
+	private static void printSolution() {
+		int sum = Arrays.stream(expression).sum();
+		if (sum == 0) {
+			solutions++;
+			for (int i = 0; i < expression.length; i++) {
+				int value = expression[i];
+
+				if (value > 0) {
+					builder.append("+" + value + " ");
+				} else {
+					builder.append(value + " ");
+				}
+			}
+			builder.append(System.lineSeparator());
+		}
 	}
 }
--- 09_exam_preparation/src/Exercise_03.java
+++ 09_exam_preparation/src/Exercise_03.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Exercise_03 {
